4497fba4acafd4690f5e0579c58ce42367358cda
Domício Medeiros <domicioam@gmail.com>
2022-04-17 11:13:57 +0100

    ...

--- src/components/createBoard/createBoard.js
+++ src/components/createBoard/createBoard.js
@@ -1,37 +1,41 @@
-import './createBoard.css';
-import { Component } from 'react';
+import "./createBoard.css";
+import { Component } from "react";
 
 class CreateBoard extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            title: ""
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+    };
 
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleChange(event) {
-        this.setState({title: event.target.value})
-    }
+  handleChange(event) {
+    this.setState({ title: event.target.value });
+  }
 
-    handleSubmit(event) {
-        this.props.createBoardApiMethod(this.state.title)
-        event.preventDefault();
-    }
+  handleSubmit(event) {
+    this.props.createBoardApiMethod(this.state.title);
+    event.preventDefault();
+  }
 
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Title:
-                    <input type="text" value={this.state.title} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Create" />
-            </form>
-        );
-    }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Title:
+          <input
+            type="text"
+            value={this.state.title}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Create" />
+      </form>
+    );
+  }
 }
 
 export default CreateBoard;
--- src/services/createBoard/api.js
+++ src/services/createBoard/api.js
@@ -1,23 +1,24 @@
-import 'whatwg-fetch'
+import "whatwg-fetch";
 
 export const createBoardApiMethod = async (title) => {
-        await window.fetch("http://localhost:8080/board", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                boardName: title
-            })
-        })
-        .then(function(response) {
-            console.log(response)
-            return response.json();
-        })
-        .then(function(json) {
-            return json;
-        })
-        .catch(function(error) {
-            console.error(error)
-        });
-}
+  await window
+    .fetch("http://localhost:8080/board", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        boardName: title,
+      }),
+    })
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (json) {
+      return json;
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+};
